91b2abcb60d9495a2777920fe8b9cca90aed344f
Masaharu Kato (Note-PC:TortoiseGit) <fudai.mk@gmail.com>
2018-06-27 10:23:50 +0900

    Added several definitions of member functions

--- AnyTuple/any_tuple.h
+++ AnyTuple/any_tuple.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <tuple>
+
 namespace mkato {
 
 	class AnyTuple {
 	public:
+	//	This type
+		using This = AnyTuple;
+
 	//	default constructor
 		AnyTuple();
 
@@ -15,14 +20,101 @@ namespace mkato {
 	//	deconstructor
 		~AnyTuple();
 
-	//	return T's value
-		template <class T> T get() const;
+	//	return T's value (const), throw exception if T doesn't exist.
+		template <class T> const T& get() const;
 
-	//	get T's value to out, return true if T exists
-		template <class T> bool get(T& out) const;
+	//	return T's value (not const), throw exception if T doesn't exist.
+		template <class T> T& get();
+
+
+	//	if T exists return T's value (const), else return default value
+		template <class T> const T& getWithDefault(const T& defval) const;
+
+	//	if T exists return T's value (not const), else return default value
+		template <class T> T& getWithDefault(const T& defval);
+
+
+	//	get T's value to out (const), return true if T exists
+		template <class T> bool get(const T& out) const;
+
+	//	get T's value to out (not const), return true if T exists
+		template <class T> bool get(T& out);
+		
+
+	//	return const pointer to T's value, return NULL if T doesn't exist.
+		template <class T> const T* getPtr() const;
+
+	//	return not const pointer to T's value, return NULL if T doesn't exist.
+		template <class T> T* getPtr();
+
+
+		
+
+	//	return several T's values (not const), throw exception if any T doesn't exist.
+		template <class... Types>
+		std::tuple<const Types&...> get() const;
+
+	//	return several T's values (const), throw exception if any T doesn't exist.
+		template <class... Types>
+		std::tuple<Types&...> get();
+
+
+	//	return several T's values (const) with default values by arguments
+		template <class... Types>
+		std::tuple<const Types&...> getWithDefault(const Types&... defvals) const;
+
+	//	return several T's values (not const) with default values by arguments
+		template <class... Types>
+		std::tuple<      Types&...> getWithDefault(const Types&... defvals);
+
+	//	return several T's values (const) with default values by standard tuple
+		template <class... Types>
+		std::tuple<const Types&...> getWithDefault(const std::tuple<Types...>& defvals) const;
+
+	//	return several T's values (not const) with default values by standard tuple
+		template <class... Types>
+		std::tuple<      Types&...> getWithDefault(const std::tuple<Types...>& defvals);
+
+	//	return several T's values (const) with default values by this type
+		template <class... Types>
+		std::tuple<const Types&...> getWithDefault(const This& defvals) const;
+
+	//	return several T's values (not const) with default values by this type
+		template <class... Types>
+		std::tuple<      Types&...> getWithDefault(const This& defvals);
+
+
+	//	get T's value to out (const), return true if T exists
+		template <class... Types>
+		bool get(const std::tuple<Types...>& out) const;
+
+	//	get T's value to out (not const), return true if T exists
+		template <class... Types>
+		bool get(std::tuple<Types...>& out);
+		
+
+	//	get T's value to out (const), return true if T exists
+		template <class... Types>
+		bool get(const Types&... outs) const;
+
+	//	get T's value to out (not const), return true if T exists
+		template <class... Types>
+		bool get(Types&... outs);
+		
+
+	//	return const pointer to T's value, return NULL if T doesn't exist.
+		template <class... Types>
+		std::tuple<const Types*...> getPtr() const;
+
+	//	return not const pointer to T's value, return NULL if T doesn't exist.
+		template <class... Types>
+		std::tuple<Types*...>  getPtr();
+
+
 
 	//	return true if T exists
 		template <class T> bool exist() const;
+
 
 	//	set T's value using copy constructor
 		template <class T> void set(const T&);
@@ -32,6 +124,7 @@ namespace mkato {
 
 	//	set T's value using direct construction (like emplace)
 		template <class T, class... Args> void emset(const Args&&...);
+
 
 	//	unset T's value
 		template <class T> void unset();
